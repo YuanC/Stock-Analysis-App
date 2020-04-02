2ad6676cd42a3750beaa2b5d8e1331d9972d3883
YuanC <jerrych.97@gmail.com>
2020-04-02 01:34:56 -0400

    as

--- resources/js/stock-query.js
+++ resources/js/stock-query.js
@@ -12,9 +12,10 @@ var app = new Vue({
       this.trades = [];
 
       setTimeout(() => {
-        Vue.http.get('/').then(response => {
+        Vue.http.post('https://848e4cc2.us-south.apigw.appdomain.cloud/stockquery/query', {"searchQuery": this.stockName}).then(response => {
 
-          // TODO: Fill out the table
+          this.trades = response.body.entries;
+          console.log(this.trades);
           this.isFetching = false;
           this.showResults = true;
 
@@ -23,6 +24,52 @@ var app = new Vue({
           this.showResults = true;
         });
       }, 500);
+    }
+  },
+  filters: {
+    msToTime: function(s){
+      function pad(n, z) {
+        z = z || 2;
+        return ('00' + n).slice(-z);
+      }
+      var ms = s % 1000;
+      s = (s - ms) / 1000;
+      var secs = s % 60;
+      s = (s - secs) / 60;
+      var mins = s % 60;
+      var hrs = (s - mins) / 60;
+
+      return pad(hrs) + ':' + pad(mins) + ':' + pad(secs);
+    },
+    dateFormat: function(s){
+      return s.substring(0,4) + "/" + s.substring(4,6) + "/" + s.substring(6)
+    },
+    symbolToExchange: function(exch){
+      var exchanges = {
+        "A": "NYSE MKT",
+        "B": "NASDAQ OMX BX",
+        "C": "National",
+        "D": "FINRA",
+        "I": "International Securities",
+        "J": "Direct Edge A",
+        "K": "Direct Edge X",
+        "M": "Chicago",
+        "N": "New York",
+        "T": "NASDAQ OMX",
+        "P": "NYSE Arca SM",
+        "S": "Consolidated Tape System",
+        "T": "NASDAQ",
+        "Q": "NASDAQ",
+        "W": "CBOE",
+        "X": "NASDAQ OMX PSX",
+        "Y": "BATS Y",
+        "Z": "BATS"
+      }
+
+      if (exch && exchanges.hasOwnProperty(exch)){
+        return exchanges[exch];
+      }
+      return '';
     }
   }
 })

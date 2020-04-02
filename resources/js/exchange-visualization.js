fbc5c3cca01feb40963a1bb6b4535c4299ba5619
Justin Koh <jkoh8@uwo.ca>
2020-04-02 09:09:56 -0400

    stock visualization

--- resources/js/exchange-visualization.js
+++ resources/js/exchange-visualization.js
@@ -15,43 +15,20 @@ var app = new Vue({
         Vue.http.get('/').then(response => {
 
           // TODO: Fill out the table
-          var ctx = document.getElementById('myChart');
+          var ctx = document.getElementById('exchangeVisualizationChart');
           var chart = new Chart(ctx, {
-            type: 'bar',
+            type: 'line',
             data: {
               labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
               datasets: [{
                 label: '# of Votes',
                 data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                  'rgba(255, 99, 132, 1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-                ],
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgba(255, 99, 132, 1)',
                 borderWidth: 1
               }]
             },
-            options: {
-              scales: {
-                yAxes: [{
-                  ticks: {
-                    beginAtZero: true
-                    }
-                  }]
-                }
-              },
-              responsive: true
+            responsive: true
           });
           this.isFetching = false;
           this.showResults = true;
--- resources/js/stock-visualization.js
+++ resources/js/stock-visualization.js
@@ -11,16 +11,30 @@ var app = new Vue({
       this.isFetching = true;
       this.trades = [];
 
-      Vue.http.get('https://848e4cc2.us-south.apigw.appdomain.cloud/stockquery/query', {"searchQuery": this.stockName}).then(response => {
+      Vue.http.post('https://848e4cc2.us-south.apigw.appdomain.cloud/stockquery/query', {"searchQuery": this.stockName}).then(response => {
           // TODO: Fill out the table
-          var ctx = document.getElementById('myChart');
+
+          console.log(response.body.entries);
+
+          this.trades = response.body.entries;
+
+          var data1 = [];
+          var data2 = []
+          for(i = 0; i < this.trades.length; i++)
+          {
+            data1[i] = this.trades[i].Price;
+            data2[i] = i;
+          }
+
+
+          var ctx = document.getElementById('stockVisualizationChart');
           var chart = new Chart(ctx, {
             type: 'line',
             data: {
-              labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+              labels: data2,
               datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
+                label: data1[0].Company,
+                data: data1,
                 backgroundColor: 'rgba(255, 99, 132, 0.2)',
                 borderColor: 'rgba(255, 99, 132, 1)',
                 borderWidth: 1
